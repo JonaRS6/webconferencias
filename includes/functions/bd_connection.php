811f7e8a343d65a271c07d8716e8a03bceca36b6
Jonathan Ruiz <jonathan.rs97@hotmail.com>
2020-09-15 15:52:07 -0500

    asdasdasd

--- includes/functions/bd_connection.php
+++ includes/functions/bd_connection.php
@@ -1,23 +1,5 @@
 <?php
-  function proxyRequest() {
-    $fixieUrl = getenv("FIXIE_URL");
-    $parsedFixieUrl = parse_url($fixieUrl);
-
-    $proxy = $parsedFixieUrl['host'].":".$parsedFixieUrl['port'];
-    $proxyAuth = $parsedFixieUrl['user'].":".$parsedFixieUrl['pass'];
-
-    $ch = curl_init('https://gdlwebcamps.herokuapp.com/');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_PROXY, $proxy);
-    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);
-    curl_close($ch);
-  }
-
-  $response = proxyRequest();
-  print_r($response);   
-?>
-<?php
-    $conn = new mysqli('localhost', 'root', '', 'gdlwebcamp', '3306');
+    $conn = new mysqli('35.222.4.81', 'root', '', 'gdlwebcamp', '3306');
 
     if ($conn->connect_error) {
         echo $error -> $conn->connect_error;
