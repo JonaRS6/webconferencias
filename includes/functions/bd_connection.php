b6c4ac60af62fa31ba2f10d9376570f560c1c492
Jonathan Ruiz <jonathan.rs97@hotmail.com>
2020-09-15 13:12:44 -0500

    Uso de Fixie

--- includes/functions/bd_connection.php
+++ includes/functions/bd_connection.php
@@ -1,4 +1,22 @@
 <?php
+  function proxyRequest() {
+    $fixieUrl = getenv("FIXIE_URL");
+    $parsedFixieUrl = parse_url($fixieUrl);
+
+    $proxy = $parsedFixieUrl['host'].":".$parsedFixieUrl['port'];
+    $proxyAuth = $parsedFixieUrl['user'].":".$parsedFixieUrl['pass'];
+
+    $ch = curl_init('https://gdlwebcamps.herokuapp.com/');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);
+    curl_close($ch);
+  }
+
+  $response = proxyRequest();
+  print_r($response);
+?>
+<?php
     $conn = new mysqli('35.222.4.81', 'root', '', 'gdlwebcamp', '3306');
 
     if ($conn->connect_error) {
